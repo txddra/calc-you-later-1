65394da24a2f36adcd9c7bcbe155618bb54034cc
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-23 14:39:13 -0400

    .

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,12 +1,19 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
-const calculate = require('./calculate-back-end.js')
+const calculate = require('./calculate-back-end.js');
+
+let num = getInput(0);
+let operator = getInput(1);
+let num2 = getInput(2)
 
 
 
+let result = calculate(num,num2, operator)
+ console.log(result)
 
 
+ 
 /******************************************
  * OUR CODE BELOW--DON'T TOUCH!           *
  *****************************************/
